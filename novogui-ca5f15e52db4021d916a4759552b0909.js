48d246110e0816b02655d0d0b1ff3013c989698c
blinx <103583498+blinxsc@users.noreply.github.com>
2022-04-19 16:33:09 +0100

    Add files via upload

---
+++ novogui-ca5f15e52db4021d916a4759552b0909.js
@@ -0,0 +1,306 @@
+// @ts-ignore
+// @ts-ignore
+// @ts-ignore
+@font-face {
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ font-family:'Roboto';
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ font-style:normal;
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ font-weight:400;
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ src:url(//fonts.gstatic.com/s/roboto/v18/KFOmCnqEu92Fr1Mu72xKOzY.woff2)format('woff2');
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ unicode-range:U+0460-052F,
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ U+1C80-1C88,
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ U+20B4,
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ U+2DE0-2DFF,
+ // @ts-ignore
+ U+A640-A69F,
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ U+FE2E-FE2F;
+}
+// @ts-ignore
+// @ts-ignore
+// @ts-ignore
+@font-face {
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ font-family:'Roboto';
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ font-style:normal;
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ font-weight:400;
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ src:url(//fonts.gstatic.com/s/roboto/v18/KFOmCnqEu92Fr1Mu5mxKOzY.woff2)format('woff2');
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ unicode-range:U+0400-045F,
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ U+0490-0491,
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ U+04B0-04B1,
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ U+2116;
+}
+// @ts-ignore
+// @ts-ignore
+// @ts-ignore
+@font-face {
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ font-family:'Roboto';
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ font-style:normal;
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ font-weight:400;
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ src:url(//fonts.gstatic.com/s/roboto/v18/KFOmCnqEu92Fr1Mu7mxKOzY.woff2)format('woff2');
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ unicode-range:U+1F00-1FFF;
+}
+// @ts-ignore
+// @ts-ignore
+// @ts-ignore
+@font-face {
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ font-family:'Roboto';
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ font-style:normal;
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ font-weight:400;
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ src:url(//fonts.gstatic.com/s/roboto/v18/KFOmCnqEu92Fr1Mu4WxKOzY.woff2)format('woff2');
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ unicode-range:U+0370-03FF;
+}
+// @ts-ignore
+// @ts-ignore
+// @ts-ignore
+@font-face {
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ font-family:'Roboto';
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ font-style:normal;
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ font-weight:400;
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ src:url(//fonts.gstatic.com/s/roboto/v18/KFOmCnqEu92Fr1Mu7WxKOzY.woff2)format('woff2');
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ unicode-range:U+0102-0103,
+ // @ts-ignore
+ U+0110-0111,
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ U+0128-0129,
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ U+0168-0169,
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ U+01A0-01A1,
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ U+01AF-01B0,
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ U+1EA0-1EF9,
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ U+20AB;
+}
+// @ts-ignore
+// @ts-ignore
+// @ts-ignore
+@font-face {
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ font-family:'Roboto';
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ font-style:normal;
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ font-weight:400;
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ src:url(//fonts.gstatic.com/s/roboto/v18/KFOmCnqEu92Fr1Mu7GxKOzY.woff2)format('woff2');
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ unicode-range:U+0100-024F,
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ U+0259,
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ U+1E00-1EFF,
+ // @ts-ignore
+ U+2020,
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ U+20A0-20AB,
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ U+20AD-20CF,
+ // @ts-ignore
+ U+2113,
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ U+2C60-2C7F,
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ U+A720-A7FF;
+}
+// @ts-ignore
+// @ts-ignore
+// @ts-ignore
+@font-face {
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ font-family:'Roboto';
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ font-style:normal;
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ font-weight:400;
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ src:url(//fonts.gstatic.com/s/roboto/v18/KFOmCnqEu92Fr1Mu4mxK.woff2)format('woff2');
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ unicode-range:U+0000-00FF,
+ // @ts-ignore
+ U+0131,
+ // @ts-ignore
+ U+0152-0153,
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ U+02BB-02BC,
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ U+02C6,
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ U+02DA,
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ U+02DC,
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ U+2000-206F,
+ // @ts-ignore
+ U+2074,
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ U+20AC,
+ // @ts-ignore
+ U+2122,
+ // @ts-ignore
+ U+2191,
+ // @ts-ignore
+ U+2193,
+ // @ts-ignore
+ U+2212,
+ // @ts-ignore
+ U+2215,
+ // @ts-ignore
+ U+FEFF,
+ // @ts-ignore
+ // @ts-ignore
+ // @ts-ignore
+ U+FFFD;
+}
